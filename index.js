39233187e86f58c5fd5a7be50d953c3c3cbbaa49
Khang Hoang <hoangtrieukhang@gmail.com>
2017-08-19 16:06:14 +0700

    clear the folder

--- index.js
+++
@@ -1,8 +0,0 @@
-import modalReducer from './src/reducer';
-
-export { close, open, toggle } from './src/actions';
-export { connectModal } from './src/connectModal';
-export { ModalManager, ModalManagerClass } from './src/modalManager';
-export { ModalPortal } from './src/portal';
-export { isModalOpenSelector, portalSelector } from './src/selector';
-export const reducer = modalReducer;
