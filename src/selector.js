4a8019763a61b49b6b8edfc77db5b00f6dce2b57
Khang Hoang <hoangtrieukhang@gmail.com>
2017-08-29 10:05:39 +0700

    fix/__DEV__ for reactjs

--- src/selector.js
+++ src/selector.js
@@ -1,4 +1,5 @@
 import { reduce } from 'lodash';
+import warning from './warning';
 
 export const isModalOpenSelector = name => state => {
   if (!(state.modals && state.modals[name])) return false;
@@ -8,12 +9,7 @@ export const isModalOpenSelector = name => state => {
 
 export const portalSelector = (gate = 'root') => state => {
   if (!state.modals) {
-    if (__DEV__) {
-      throw new Error(
-        'You need to register portal modal reducer to root reducer'
-      );
-    }
-
+    warning('You need to register portal modal reducer to root reducer');
     return {};
   }
 
